56014c740eec77118775e01449b49466fa8e597f
SameButDifferent <76499872+samedifferent@users.noreply.github.com>
2021-10-07 12:47:51 -0400

    Fix climbing effect

--- src/main/java/samebutdifferent/trickortreat/effect/ClimbingEffect.java
+++ src/main/java/samebutdifferent/trickortreat/effect/ClimbingEffect.java
@@ -3,6 +3,7 @@ package samebutdifferent.trickortreat.effect;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.phys.Vec3;
 
 public class ClimbingEffect extends MobEffect {
     public ClimbingEffect() {
@@ -11,6 +12,11 @@ public class ClimbingEffect extends MobEffect {
 
     @Override
     public void applyEffectTick(LivingEntity entity, int amplifier) {
+        if (entity.horizontalCollision) {
+            Vec3 vec3 = entity.getDeltaMovement();
+            Vec3 movement = new Vec3(vec3.x, 0.2D, vec3.z);
+            entity.setDeltaMovement(movement.x * 0.91D, movement.y * 0.98D, movement.z * 0.91D);
+        }
     }
 
     @Override
