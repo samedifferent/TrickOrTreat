35dd8c7ee4720d889c2e5916142c3d30b4838bd9
SameButDifferent <76499872+samedifferent@users.noreply.github.com>
2021-10-07 13:29:26 -0400

    Add spider sound effects

--- src/main/java/samebutdifferent/trickortreat/effect/ClimbingEffect.java
+++ src/main/java/samebutdifferent/trickortreat/effect/ClimbingEffect.java
@@ -1,11 +1,22 @@
 package samebutdifferent.trickortreat.effect;
 
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.phys.Vec3;
+import net.minecraftforge.client.event.EntityViewRenderEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import samebutdifferent.trickortreat.TrickOrTreat;
+import samebutdifferent.trickortreat.registry.ModEffects;
 
+@Mod.EventBusSubscriber(modid = TrickOrTreat.MOD_ID)
 public class ClimbingEffect extends MobEffect {
+    private int ambientSoundTime;
+
     public ClimbingEffect() {
         super(MobEffectCategory.BENEFICIAL, 3801088);
     }
@@ -16,6 +27,10 @@ public class ClimbingEffect extends MobEffect {
             Vec3 vec3 = entity.getDeltaMovement();
             Vec3 movement = new Vec3(vec3.x, 0.2D, vec3.z);
             entity.setDeltaMovement(movement.x * 0.91D, movement.y * 0.98D, movement.z * 0.91D);
+        }
+        if (entity.getRandom().nextInt(1000) < this.ambientSoundTime++) {
+            this.ambientSoundTime = -40;
+            entity.playSound(SoundEvents.SPIDER_AMBIENT, 1.0F, 1.0F);
         }
     }
 
