cde48b73361ea4186c18d6a741390430ec167c1c
SameButDifferent <76499872+samedifferent@users.noreply.github.com>
2021-10-09 19:37:13 -0400

    Fix paralyzed effect

--- src/main/java/samebutdifferent/trickortreat/effect/ParalyzedEffect.java
+++ src/main/java/samebutdifferent/trickortreat/effect/ParalyzedEffect.java
@@ -1,25 +1,49 @@
 package samebutdifferent.trickortreat.effect;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.player.Input;
+import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.phys.Vec3;
+import net.minecraftforge.client.event.InputUpdateEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import samebutdifferent.trickortreat.TrickOrTreat;
+import samebutdifferent.trickortreat.registry.ModEffects;
 
+@Mod.EventBusSubscriber(modid = TrickOrTreat.MOD_ID)
 public class ParalyzedEffect extends MobEffect {
     public ParalyzedEffect() {
         super(MobEffectCategory.HARMFUL, 16777038);
-        this.addAttributeModifier(Attributes.MOVEMENT_SPEED, "91AEAA56-376B-4498-935B-2F7F68070636", -1.0, AttributeModifier.Operation.MULTIPLY_TOTAL);
     }
 
     @Override
     public void applyEffectTick(LivingEntity entity, int amplifier) {
-        entity.setJumping(false);
+        entity.setDeltaMovement(Vec3.ZERO);
     }
 
     @Override
     public boolean isDurationEffectTick(int pDuration, int pAmplifier) {
         return true;
+    }
+
+    @SubscribeEvent
+    public static void onInputUpdate(InputUpdateEvent event) {
+        Input input = event.getMovementInput();
+        if (event.getPlayer().hasEffect(ModEffects.PARALYZED.get())) {
+            input.up = false;
+            input.down = false;
+            input.left = false;
+            input.right = false;
+            input.forwardImpulse = 0;
+            input.leftImpulse = 0;
+            input.jumping = false;
+            input.shiftKeyDown = false;
+        }
     }
 }
