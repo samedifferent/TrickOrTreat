d6e60901c2500b041e2bc84f98b5825a0a8e2e34
SameButDifferent <76499872+samedifferent@users.noreply.github.com>
2021-10-09 13:15:02 -0400

    Paralyzed

--- src/main/java/samebutdifferent/trickortreat/effect/ParalyzedEffect.java
+++ src/main/java/samebutdifferent/trickortreat/effect/ParalyzedEffect.java
@@ -3,16 +3,19 @@ package samebutdifferent.trickortreat.effect;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.phys.Vec3;
 
 public class ParalyzedEffect extends MobEffect {
     public ParalyzedEffect() {
         super(MobEffectCategory.HARMFUL, 16777038);
+        this.addAttributeModifier(Attributes.MOVEMENT_SPEED, "91AEAA56-376B-4498-935B-2F7F68070636", -1.0, AttributeModifier.Operation.MULTIPLY_TOTAL);
     }
 
     @Override
     public void applyEffectTick(LivingEntity entity, int amplifier) {
-        entity.setDeltaMovement(Vec3.ZERO);
+        entity.setJumping(false);
     }
 
     @Override
