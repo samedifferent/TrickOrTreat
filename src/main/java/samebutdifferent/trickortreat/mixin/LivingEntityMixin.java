dd3e8b281c7e8d1162729dfcf0831b0cb850bcdc
Jamalam360 <56727311+Jamalam360@users.noreply.github.com>
2021-10-14 18:48:20 +0100

    Port LivingDropEvent to mixin

--- src/main/java/samebutdifferent/trickortreat/mixin/LivingEntityMixin.java
+++ src/main/java/samebutdifferent/trickortreat/mixin/LivingEntityMixin.java
@@ -1,14 +1,25 @@
 package samebutdifferent.trickortreat.mixin;
 
+import me.shedaniel.autoconfig.AutoConfig;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.mob.*;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.sound.SoundEvents;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import samebutdifferent.trickortreat.TrickOrTreatModInit;
+import samebutdifferent.trickortreat.registry.ModConfig;
 import samebutdifferent.trickortreat.registry.ModEffects;
+import samebutdifferent.trickortreat.registry.ModItems;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Jamalam360
@@ -22,7 +33,6 @@ public class LivingEntityMixin {
     )
     public void damageMixin(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
         //region Life Leech
-
         if (source.getSource() instanceof PlayerEntity player && player.hasStatusEffect(ModEffects.LIFE_LEECH)) {
             float healthStealAmount = Math.min(6, amount / 4);
             if (healthStealAmount >= 1) {
@@ -30,7 +40,71 @@ public class LivingEntityMixin {
                 player.heal(healthStealAmount);
             }
         }
-
         //endregion
+    }
+
+    @Inject(
+            at = @At("HEAD"),
+            method = "dropLoot"
+    )
+    public void dropLootMixin(DamageSource source, boolean causedByPlayer, CallbackInfo ci) {
+        ModConfig config = AutoConfig.getConfigHolder(ModConfig.class).getConfig();
+        List<ItemStack> dropStacks = new ArrayList<>();
+
+        if (TrickOrTreatModInit.isHalloween() || !config.onlyHalloween) {
+            if (!source.isFromFalling() && !source.isFire() && !source.isMagic()) {
+                Entity entity = source.getSource();
+
+                if (entity.world.random.nextFloat() <= 0.25F * config.goodieBagDropChance) {
+                    if (entity instanceof BlazeEntity) {
+                        dropStacks.add(new ItemStack(ModItems.BLAZE_GOODIE_BAG));
+                    }
+
+                    if (entity instanceof CreeperEntity) {
+                        dropStacks.add(new ItemStack(ModItems.CREEPER_GOODIE_BAG));
+                    }
+
+                    if (entity instanceof DrownedEntity) {
+                        dropStacks.add(new ItemStack(ModItems.DROWNED_GOODIE_BAG));
+                    }
+
+                    if (entity instanceof EndermanEntity) {
+                        dropStacks.add(new ItemStack(ModItems.ENDERMAN_GOODIE_BAG));
+                    }
+
+                    if (entity instanceof GhastEntity) {
+                        dropStacks.add(new ItemStack(ModItems.GHAST_GOODIE_BAG));
+                    }
+
+                    if (entity instanceof GuardianEntity) {
+                        dropStacks.add(new ItemStack(ModItems.GUARDIAN_GOODIE_BAG));
+                    }
+
+                    if (entity instanceof PhantomEntity) {
+                        dropStacks.add(new ItemStack(ModItems.PHANTOM_GOODIE_BAG));
+                    }
+
+                    if (entity instanceof SkeletonEntity) {
+                        dropStacks.add(new ItemStack(ModItems.SKELETON_GOODIE_BAG));
+                    }
+
+                    if (entity instanceof SlimeEntity) {
+                        dropStacks.add(new ItemStack(ModItems.SLIME_GOODIE_BAG));
+                    }
+
+                    if (entity instanceof SpiderEntity) {
+                        dropStacks.add(new ItemStack(ModItems.SPIDER_GOODIE_BAG));
+                    }
+
+                    if (entity instanceof ZombieEntity) {
+                        dropStacks.add(new ItemStack(ModItems.ZOMBIE_GOODIE_BAG));
+                    }
+                }
+            }
+        }
+
+        for (ItemStack drop : dropStacks) {
+            ((LivingEntity) (Object) this).dropStack(drop);
+        }
     }
 }
--- src/main/java/samebutdifferent/trickortreat/registry/ModEvents.java
+++ src/main/java/samebutdifferent/trickortreat/registry/ModEvents.java
@@ -18,42 +18,6 @@ import net.minecraft.util.ActionResult;
 import samebutdifferent.trickortreat.mixin.MobEntityAccessor;
 
 public class ModEvents implements RegistryClass {
-    //TODO: Port this to a mixin!
-    /*@SubscribeEvent
-    static void onMobDrops(LivingDropsEvent event) {
-        LivingEntity entity = event.getEntityLiving();
-        Level level = entity.level;
-        if (TrickOrTreat.isHalloween() || !ModConfig.ONLY_HALLOWEEN.get()) {
-            if (!event.getSource().isFall() && !event.getSource().isFire() && !event.getSource().isMagic()) {
-                if (level.random.nextFloat() <= 0.25F * ModConfig.GOODIE_BAG_DROP_CHANCE.get().floatValue()) {
-                    if (entity instanceof Blaze)
-                        event.getDrops().add(new ItemEntity(level, entity.getX(), entity.getY(), entity.getZ(), new ItemStack(ModItems.BLAZE_GOODIE_BAG.get())));
-                    if (entity instanceof Creeper)
-                        event.getDrops().add(new ItemEntity(level, entity.getX(), entity.getY(), entity.getZ(), new ItemStack(ModItems.CREEPER_GOODIE_BAG.get())));
-                    if (entity instanceof Drowned)
-                        event.getDrops().add(new ItemEntity(level, entity.getX(), entity.getY(), entity.getZ(), new ItemStack(ModItems.DROWNED_GOODIE_BAG.get())));
-                    if (entity instanceof EnderMan)
-                        event.getDrops().add(new ItemEntity(level, entity.getX(), entity.getY(), entity.getZ(), new ItemStack(ModItems.ENDERMAN_GOODIE_BAG.get())));
-                    if (entity instanceof Ghast)
-                        event.getDrops().add(new ItemEntity(level, entity.getX(), entity.getY(), entity.getZ(), new ItemStack(ModItems.GHAST_GOODIE_BAG.get())));
-                    if (entity instanceof Guardian)
-                        event.getDrops().add(new ItemEntity(level, entity.getX(), entity.getY(), entity.getZ(), new ItemStack(ModItems.GUARDIAN_GOODIE_BAG.get())));
-                    if (entity instanceof Phantom)
-                        event.getDrops().add(new ItemEntity(level, entity.getX(), entity.getY(), entity.getZ(), new ItemStack(ModItems.PHANTOM_GOODIE_BAG.get())));
-                    if (entity instanceof Skeleton)
-                        event.getDrops().add(new ItemEntity(level, entity.getX(), entity.getY(), entity.getZ(), new ItemStack(ModItems.SKELETON_GOODIE_BAG.get())));
-                    if (entity instanceof Slime)
-                        event.getDrops().add(new ItemEntity(level, entity.getX(), entity.getY(), entity.getZ(), new ItemStack(ModItems.SLIME_GOODIE_BAG.get())));
-                    if (entity instanceof Spider)
-                        event.getDrops().add(new ItemEntity(level, entity.getX(), entity.getY(), entity.getZ(), new ItemStack(ModItems.SPIDER_GOODIE_BAG.get())));
-                    if (entity instanceof Zombie)
-                        event.getDrops().add(new ItemEntity(level, entity.getX(), entity.getY(), entity.getZ(), new ItemStack(ModItems.ZOMBIE_GOODIE_BAG.get())));
-                }
-            }
-        }
-    }*/
-
-    @SuppressWarnings("ReferenceToMixin")
     @Override
     public void register(String modId) {
         AttackEntityCallback.EVENT.register(((player, world, hand, entity, hitResult) -> {
