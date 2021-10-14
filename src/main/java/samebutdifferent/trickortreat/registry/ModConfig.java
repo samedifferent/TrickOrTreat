b36528eec9327523c143ca7e23dacd5e9c7de976
Jamalam360 <56727311+Jamalam360@users.noreply.github.com>
2021-10-14 19:33:59 +0100

    Finish polishing config

--- src/main/java/samebutdifferent/trickortreat/registry/ModConfig.java
+++ src/main/java/samebutdifferent/trickortreat/registry/ModConfig.java
@@ -7,19 +7,8 @@ import samebutdifferent.trickortreat.TrickOrTreatModInit;
 
 @Config(name = TrickOrTreatModInit.MOD_ID)
 public class ModConfig implements ConfigData {
-    //TODO: Port Config to Auto/Cloth Config
-
-    //@ConfigEntry.Gui.Tooltip
+    @ConfigEntry.Gui.Tooltip(count = 2)
     public boolean onlyHalloween = true;
-    @ConfigEntry.Gui.Tooltip
+    @ConfigEntry.Gui.Tooltip(count = 2)
     public double goodieBagDropChance = 1.0;
-
-    /*static {
-        ForgeConfigSpec.Builder COMMON_BUILDER = new ForgeConfigSpec.Builder();
-
-        ONLY_HALLOWEEN = COMMON_BUILDER.comment("Should goodie bags only drop during Halloween month? (October 1 to November 1)")
-                .define("onlyHalloween", true);
-        GOODIE_BAG_DROP_CHANCE = COMMON_BUILDER.comment("Multiply the chance that goodie bags drop")
-                .defineInRange("goodieBagDropChance", 1.0, 0.1, 4.0);
-    }*/
 }
